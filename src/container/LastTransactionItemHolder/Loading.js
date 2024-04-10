2a9da48e0afaaf62df881d23ded111577e137bee
Batas <nischal.batas88@gmail.com>
2024-04-10 22:00:27 +0545

    adding skeleton on profile page

--- src/container/LastTransactionItemHolder/Loading.js
+++ src/container/LastTransactionItemHolder/Loading.js
@@ -52,6 +52,132 @@ const Loading = () => {
           </div>
         </div>
       </div>
+
+      <div className="py-2 px-1 gap-1  flex justify-start item-center bg-anovanavbar rounded">
+        <div className="flex items-center justify-center w-10 h-8 rounded-full bg-gray-300 dark:bg-gray-700">
+          <svg
+            class="w-4 h-4 text-gray-200 dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 16 20"
+          >
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+          </svg>
+        </div>
+        <div className="flex justify-between w-full items-center px-2 gap-2">
+          <div>
+            <div class="h-2 w-16 bg-gray-200 rounded dark:bg-gray-700 mb-2"></div>
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="py-2 px-1 gap-1  flex justify-start item-center bg-anovanavbar rounded">
+        <div className="flex items-center justify-center w-10 h-8 rounded-full bg-gray-300 dark:bg-gray-700">
+          <svg
+            class="w-4 h-4 text-gray-200 dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 16 20"
+          >
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+          </svg>
+        </div>
+        <div className="flex justify-between w-full items-center px-2 gap-2">
+          <div>
+            <div class="h-2 w-16 bg-gray-200 rounded dark:bg-gray-700 mb-2"></div>
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-2 px-1 gap-1  flex justify-start item-center bg-anovanavbar rounded">
+        <div className="flex items-center justify-center w-10 h-8 rounded-full bg-gray-300 dark:bg-gray-700">
+          <svg
+            class="w-4 h-4 text-gray-200 dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 16 20"
+          >
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+          </svg>
+        </div>
+        <div className="flex justify-between w-full items-center px-2 gap-2">
+          <div>
+            <div class="h-2 w-16 bg-gray-200 rounded dark:bg-gray-700 mb-2"></div>
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+        </div>
+      </div>
+      <div className="py-2 px-1 gap-1  flex justify-start item-center bg-anovanavbar rounded">
+        <div className="flex items-center justify-center w-10 h-8 rounded-full bg-gray-300 dark:bg-gray-700">
+          <svg
+            class="w-4 h-4 text-gray-200 dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 16 20"
+          >
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+          </svg>
+        </div>
+        <div className="flex justify-between w-full items-center px-2 gap-2">
+          <div>
+            <div class="h-2 w-16 bg-gray-200 rounded dark:bg-gray-700 mb-2"></div>
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-2 px-1 gap-1  flex justify-start item-center bg-anovanavbar rounded">
+        <div className="flex items-center justify-center w-10 h-8 rounded-full bg-gray-300 dark:bg-gray-700">
+          <svg
+            class="w-4 h-4 text-gray-200 dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 16 20"
+          >
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+          </svg>
+        </div>
+        <div className="flex justify-between w-full items-center px-2 gap-2">
+          <div>
+            <div class="h-2 w-16 bg-gray-200 rounded dark:bg-gray-700 mb-2"></div>
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div class="h-2 w-10  bg-gray-200 rounded dark:bg-gray-700"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
